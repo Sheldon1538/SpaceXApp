e8c4e17088dc9fe32da09138da99211c5f60ef41
Konstantin Akchurin <sheldon1538@yahoo.com>
2020-03-13 22:52:00 +0300

    - Reorganised project structure;

--- SpaceXApp/Controller/LatestNews/LatestViewController.swift
+++ SpaceXApp/Controller/LatestNews/LatestViewController.swift
@@ -8,13 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LatestViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
 }
-
