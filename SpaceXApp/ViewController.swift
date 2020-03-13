7dd76e80844fdfb172f0edb550e013079038ab40
Konstantin Akchurin <sheldon1538@yahoo.com>
2020-03-13 21:59:30 +0300

    Initial Commit

---
+++ SpaceXApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SpaceXApp
+//
+//  Created by Konstantin on 13/03/2020.
+//  Copyright © 2020 Konstantin Akchurin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
