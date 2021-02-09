89697d7ef5cda41e2d35f5fdff3b8a304c1bfe6c
Konstantin Akchurin <sheldon1538@yahoo.com>
2021-02-09 15:32:14 +0300

    Add rocket preview cell

--- SpaceXApp/Controller/Rockets/RocketsViewController.swift
+++ SpaceXApp/Controller/Rockets/RocketsViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class RocketsViewController: UIViewController {
 
+    var collectionView: UICollectionView!
+    
     init() {
         super.init(nibName: nil, bundle: nil)
     }
@@ -20,6 +22,49 @@ class RocketsViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .orange
+        title = "Rockets"
+        view.backgroundColor = .white
+        setupCollectionViewFlowLayout()
+        addCollectionView()
+        collectionView.register(RocketPreviewCollectionViewCell.self, forCellWithReuseIdentifier: RocketPreviewCollectionViewCell.identifier)
     }
+    
+    func setupCollectionViewFlowLayout() {
+        let layout = UICollectionViewFlowLayout()
+        collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.backgroundColor = .clear
+    }
+    
+    func addCollectionView() {
+        view.addSubview(collectionView)
+        collectionView.anchor(top: view.safeAreaLayoutGuide.topAnchor, leading: view.safeAreaLayoutGuide.leadingAnchor, bottom: view.bottomAnchor, trailing: view.safeAreaLayoutGuide.trailingAnchor)
+    }
+}
+
+extension RocketsViewController: UICollectionViewDelegate, UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: RocketPreviewCollectionViewCell.identifier, for: indexPath) as? RocketPreviewCollectionViewCell else { return UICollectionViewCell() }
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return .init(width: UIScreen.main.bounds.width - 32.0, height: 200.0)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 24.0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 16, left: 0, bottom: 8, right: 0)
+    }
+    
 }
---
+++ SpaceXApp/View/UICollectionViewCells/Rocket preview/RocketPreviewCollectionViewCell.swift
@@ -0,0 +1,122 @@
+//
+//  RocketPreviewCollectionViewCell.swift
+//  SpaceXApp
+//
+//  Created by Konstantin on 05.02.2021.
+//  Copyright © 2021 Konstantin Akchurin. All rights reserved.
+//
+
+import UIKit
+
+class RocketPreviewCollectionViewCell: UICollectionViewCell {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupRocketImageView()
+        setupRocketDetailsBackgroundView()
+        setupRocketDetailsStackView()
+        
+        addShadow(to: rocketImageView)
+        addShadow(to: rocketDetailsBackgroundView)
+        
+        contentView.bringSubviewToFront(rocketImageView)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    class var identifier: String {
+        return String(describing: self)
+    }
+    
+    let rocketNameLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .left
+        label.textColor = .black
+        label.font = UIFont.systemFont(ofSize: 18, weight: .black)
+        return label
+    }()
+    
+    let rocketDescriptionLabel: DetailedInfoLabel = {
+       let label = DetailedInfoLabel()
+        label.numberOfLines = 3
+        label.font = UIFont.systemFont(ofSize: 14, weight: .semibold)
+        return label
+    }()
+    
+    let rocketEnginesLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .left
+        label.textColor = #colorLiteral(red: 0.9451864844, green: 0.7608078703, blue: 0.6433241656, alpha: 0.85)
+        label.font = UIFont.systemFont(ofSize: 14, weight: .medium)
+        return label
+    }()
+    
+    let firstLaunchLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .left
+        label.textColor = .black
+        label.font = UIFont.systemFont(ofSize: 14, weight: .bold)
+        return label
+    }()
+    
+    let rocketImageView: UIImageView = {
+       let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFit
+        imageView.backgroundColor = .lightGray
+        imageView.layer.cornerRadius = 16.0
+        return imageView
+    }()
+    
+    let rocketDetailsBackgroundView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .white
+        view.layer.cornerRadius = 12.0
+        return view
+    }()
+    
+    let rocketDetailsStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.distribution = .fillProportionally
+        return stackView
+    }()
+    
+    private func setupRocketImageView() {
+        contentView.addSubview(rocketImageView)
+        rocketImageView.anchor(top: contentView.topAnchor, leading: contentView.leadingAnchor, bottom: nil, trailing: nil, size: .init(width: 130, height: 170))
+    }
+    
+    private func setupRocketDetailsBackgroundView() {
+        contentView.addSubview(rocketDetailsBackgroundView)
+        rocketDetailsBackgroundView.anchor(top: rocketImageView.topAnchor, leading: rocketImageView.leadingAnchor, bottom: contentView.bottomAnchor, trailing: contentView.trailingAnchor, padding: .init(top: 16.0, left: 16.0, bottom: 0.0, right: 0.0))
+    }
+    
+    private func setupRocketDetailsStackView() {
+        contentView.addSubview(rocketDetailsStackView)
+        rocketDetailsStackView.anchor(top: rocketDetailsBackgroundView.topAnchor, leading: rocketImageView.trailingAnchor, bottom: rocketDetailsBackgroundView.bottomAnchor, trailing: rocketDetailsBackgroundView.trailingAnchor, padding: .init(top: 8.0, left: 16.0, bottom: 16.0, right: 16.0))
+        
+        rocketDetailsStackView.addArrangedSubview(rocketNameLabel)
+        rocketDetailsStackView.addArrangedSubview(rocketEnginesLabel)
+        rocketDetailsStackView.addArrangedSubview(firstLaunchLabel)
+        rocketDetailsStackView.addArrangedSubview(rocketDescriptionLabel)
+        
+        rocketNameLabel.text = "Falcon 1"
+        rocketEnginesLabel.text = "1 engine"
+        firstLaunchLabel.text = "24 March, 2006"
+        rocketDescriptionLabel.text = "The Falcon 1 was an expendable launch system privately developed and manufactured by SpaceX during 2006-2009. On 28 September 2008, Falcon 1 became the first privately-developed liquid-fuel launch vehicle to go into orbit around the Earth."
+    }
+    
+    private func addShadow(to view: UIView) {
+        view.layer.shadowColor = UIColor.black.cgColor
+        view.layer.shadowOpacity = 0.07
+        view.layer.shadowOffset = .zero
+        view.layer.shadowRadius = 8
+    }
+}
